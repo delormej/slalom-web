89877d9009b552c1ff82e6a853cdbca24bcfe77d
Jason De Lorme <jasondel@microsoft.com>
2019-07-15 17:11:43 -0700

    Refactoring to use state and support updating input boxes.

--- src/Video.js
+++ src/Video.js
@@ -4,43 +4,56 @@ export default class Video extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.state = {
-        video: this.props.video
+        video: this.props.video,
+        ropeLengthM: this.props.video.ropeLengthM || 15,
+        skier: this.props.video.skier || ''
       };
   }
 
-  getImageUrl(video) {
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+        [name]: value
+    });
+  }
+
+  getImageUrl() {
     var baseUrl = 'https://ski-app.azurewebsites.net/api/image?jsonUrl='; 
-    var imageUrl = baseUrl + video.jsonUrl;
-    if (video.ropeLengthM != null && video.ropeLengthM !== "0.0") {
-        imageUrl += '&rope=' + video.ropeLengthM;
+    var imageUrl = baseUrl + this.state.video.jsonUrl;
+    if (this.state.video.ropeLengthM != null && this.state.ropeLengthM !== "0.0") {
+        imageUrl += '&rope=' + this.state.ropeLengthM;
     }
     return imageUrl;
   }
 
-  getVideoUrl(video) {
+  getVideoUrl() {
       var baseUrl = './video.html?video=';
-      var videoUrl = baseUrl + video.url;
+      var videoUrl = baseUrl + this.state.video.url;
       return videoUrl;
   }
 
-  getThumnailUrl(video) {
-      return video.thumbnailUrl;
+  getThumnailUrl() {
+      return this.state.video.thumbnailUrl;
   }
 
   render() {
     var video = this.state.video;      
     return (
       <div>
-          <a href={this.getThumnailUrl(video)} target="_blank">
-              <img src={this.getThumnailUrl(video)} width="320" height="240"/>    
+          <a href={this.getThumnailUrl()} target="_blank">
+              <img src={this.getThumnailUrl()} width="320" height="240"/>    
           </a><br/>
-          <a href={this.getVideoUrl(video)} target="_blank">Video</a><br/>
-          <a href={this.getImageUrl(video)} target="_blank">Analysis</a><br/>
+          <a href={this.getVideoUrl()} target="_blank">Video</a><br/>
+          <a href={this.getImageUrl()} target="_blank">Analysis</a><br/>
+          <b>Date:</b>{video.partitionKey}<br/>
           <b>Course Name:</b>{video.courseName}<br/>
           <b>Speed:</b>{video.boatSpeedMph}<br/>
-          <b>Skier:</b><input type="text" id="skier"/><br/>
-          <b>Rope Length:</b><input type="select" id="ropeLengthM"/><br/>
+          <b>Skier:</b><input type="text" value={this.state.skier} onChange={this.handleInputChange} name="skier"/><br/>
+          <b>Rope Length:</b><input type="text" value={this.state.ropeLengthM} onChange={this.handleInputChange} name="ropeLengthM"/><br/>
           <hr/>
       </div>
     );
