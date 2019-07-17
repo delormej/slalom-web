4b51df68c0b656b57692560bb07843f5c421c209
Jason De Lorme <jasondel@microsoft.com>
2019-07-17 15:09:27 -0700

    Updating conditional version check, include build number variable if run from CI, else write local.

--- write-version.sh
+++ write-version.sh
@@ -1,1 +1,3 @@
-echo "REACT_APP_VERSION=\${npm_package_version}-$(git rev-parse HEAD | cut -c 1-8)-\$BUILD_BUILDNUMBER" > .env
+#!/bin/bash
+BUILD_BUILDNUMBER=${BUILD_BUILDNUMBER:=local} 
+echo "REACT_APP_VERSION=\${npm_package_version}-$(git rev-parse HEAD | cut -c 1-8)-$BUILD_BUILDNUMBER" > .env
