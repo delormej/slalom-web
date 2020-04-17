2e61b6eab76c0062fb9ad6ffb59d86c8c116fc2f
Jason De Lorme <jjdelorme@yahoo.com>
2020-04-17 10:55:14 -0400

    Promoted to v1.2 and updated deployment.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
+VERSION=v$(node -p "require('./package.json').version")
 npm run clean
-docker build -t skiweb:v1.1.0 --build-arg skiapi_host=api.ski.jasondel.com -f Dockerfile . 
-docker run --rm --name skiweb1 -e PORT=3000 -p 3000:3000 skiweb:v1.1.0   
+docker build -t skiweb:$VERSION --build-arg skiapi_host=api.ski.jasondel.com -f Dockerfile . 
+docker run --rm --name skiweb1 -e PORT=3000 -p 3000:3000 skiweb:$VERSION
 
-docker tag skiweb:v1.1.0 wthacr.azurecr.io/skiweb:v1.1.0
-docker push wthacr.azurecr.io/skiweb:v1.1.0
+docker tag skiweb:$VERSION wthacr.azurecr.io/skiweb:$VERSION
+docker push wthacr.azurecr.io/skiweb:$VERSION
