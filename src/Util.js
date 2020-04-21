1c23aa89e7eb1a52859a46b221171f057a59746a
Jason De Lorme <jjdelorme@yahoo.com>
2020-04-21 07:48:29 -0400

    Defaulting to new api server.

--- src/Util.js
+++ src/Util.js
@@ -2,7 +2,7 @@ export default class Util {
 
   getBaseUrl() {
     // sets the appropriate baseurl based on whether we're called via ssl or not (https:// or http://)
-    const defaultApiHostname = process.env.REACT_APP_SKIAPI_HOST || 'ski-app.azurewebsites.net'; 
+    const defaultApiHostname = process.env.REACT_APP_SKIAPI_HOST || 'ski.jasondel.com'; 
     var baseUrl = '';
     
     if (typeof window !== 'undefined') {
