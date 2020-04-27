0e76b973ea2ec5a1a0cf6176a0ef6db173b3f995
Jason De Lorme <jjdelorme@yahoo.com>
2020-04-27 08:20:32 -0400

    Adding extra check for empty string.

--- src/Util.js
+++ src/Util.js
@@ -1,12 +1,13 @@
 export default class Util {
 
   getBaseUrl() {
+    // NOTE: This value only works at build time, it's written statically during the build.
     const defaultApiHostname = process.env.REACT_APP_SKIAPI_HOST; 
     // Default if not overide, just returns empty, and app will use relative to current request.
     var baseUrl = '';
     
     // If a hostname was provided, match the protocol (http/https).
-    if (defaultApiHostname !== undefined) {
+    if (defaultApiHostname !== undefined && defaultApiHostname !== '') {
       if (typeof window !== 'undefined') {
         baseUrl = window.location.protocol + '//' + defaultApiHostname;
       }
