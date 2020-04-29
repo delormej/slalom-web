404ceb88c4e4e211d7dfc7652ea7ef6a833feb2d
Jason De Lorme <jjdelorme@yahoo.com>
2020-04-28 20:36:07 -0400

    Adding acr login to build script.

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ echo "Go ahead and test..."
 docker run --rm --name skiweb1 -e PORT=3000 -p 3000:3000 skiweb:$VERSION
 
 # Now push container to ACR.
+az acr login -n wthacr
 docker tag skiweb:$VERSION wthacr.azurecr.io/skiweb:$VERSION
 docker push wthacr.azurecr.io/skiweb:$VERSION
