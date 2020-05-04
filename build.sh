fbf148fabd7038d904757ddde650f02bfcddaed4
Jason De Lorme <jjdelorme@yahoo.com>
2020-05-04 15:57:21 -0400

    Updating build script.

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ VERSION=v$(node -p "require('./package.json').version")
 
 docker build -t skiweb:$VERSION --build-arg skiapi_host=$SKIAPI_HOST -f Dockerfile . 
 
-echo "Go ahead and test..."
-docker run --rm --name skiweb1 -e PORT=3000 -p 3000:3000 skiweb:$VERSION
+#echo "Go ahead and test..."
+#docker run --rm --name skiweb1 -e PORT=3000 -p 3000:3000 skiweb:$VERSION
 
 # Now push container to ACR.
 az acr login -n wthacr
